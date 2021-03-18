d79bd3830239c1d8ffc003d84f37455b3b3a08b4
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-03-18 21:33:25 +0100

    remove ts-ignore

--- src/components/hierarchy-table/hierarchy-table.component.ts
+++ src/components/hierarchy-table/hierarchy-table.component.ts
@@ -1,30 +1,22 @@
+import {Options, Vue} from "vue-class-component";
 import ProductService from '@/service/ProductService';
 
+export default class HierarchyTable extends Vue {
+	columns: any[] = [];
+	products: any[] = [];
+	productService: ProductService = new ProductService();
 
-export default {
-	data() {
-		return {
-			columns: null,
-			products: null
-		}
-	},
 	created() {
-		// @ts-ignore
-		this.productService = new ProductService();
-
-		// @ts-ignore
 		this.columns = [
 			{field: 'code', header: 'Code'},
 			{field: 'name', header: 'Name'},
 			{field: 'category', header: 'Category'},
 			{field: 'quantity', header: 'Quantity'}
 		];
-	},
+	}
 
 	mounted() {
-		// @ts-ignore
 		this.productService.getProductsSmall().then(data => {
-			// @ts-ignore
 			this.products = data
 		});
 	}
