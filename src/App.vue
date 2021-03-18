84a0f17ff143df8480367829b0deaa7448c990f4
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-03-18 20:33:41 +0100

    integrate primevue example

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <HierarchyTable msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="app">
+    <div class="app-container">
+      <HierarchyTable msg="Welcome to Your PrimeVue App"/>
+      <form @submit.prevent="greet">
+        <InputText type="text" v-model="text"/>
+        <Button type="submit" label="Submit"/>
+        <h3>{{ message }}</h3>
+      </form>
+    </div>
+
+    <Toast/>
+  </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HierarchyTable from "./components/hierarchy-table/hierarchy-table.vue";
+<script>
+import HierarchyTable from './components/hierarchy-table/hierarchy-table'
 
-@Options({
+export default {
+  data() {
+    return {
+      message: null,
+      text: null
+    }
+  },
+  methods: {
+    greet() {
+      this.$toast.add({severity: 'info', summary: 'Hello ' + this.text});
+      this.message = 'Hello ' + this.text;
+    }
+  },
   components: {
     HierarchyTable
   }
-})
-export default class App extends Vue {}
+}
 </script>
 
-<style lang="scss">
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.app-container {
+  text-align: center;
+}
+
+body #app .p-button {
+  margin-left: .2em;
+}
+
+form {
+  margin-top: 2em;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,23 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.use(ToastService);
+
+app.component('InputText', InputText);
+app.component('Button', Button);
+app.component('Toast', Toast);
+
+app.mount('#app')
